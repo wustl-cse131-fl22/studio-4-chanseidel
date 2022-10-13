77305e6001e3af4c6524d7fa0232cdcb8875c0c6
tracy <tracy@localhost>
2022-10-13 13:26:07 -0500

    testing

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,24 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		System.out.print(shape);
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean fill = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeigh = in.nextDouble();
+		Color color1 = new Color(redComponent,greenComponent,blueComponent);
+		StdDraw.setPenColor(color1);
+		if (fill) {
+			StdDraw.filledRectangle(x, y, halfWidth, halfHeigh);
+		} else {
+			StdDraw.rectangle(x, y, halfWidth, halfHeigh);
+		}
 		
 	}
 }
